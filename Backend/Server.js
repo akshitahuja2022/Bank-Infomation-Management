81021189ed4d21b2926bae49084c69def1134eea
Akshit Ahuja <akshitahuja1322@gmail.com>
2025-07-23 14:05:57 +0530

    Cors issue

--- Backend/Server.js
+++ Backend/Server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://bankinfomanage.netlify.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
